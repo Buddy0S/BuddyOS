9bac7d4b849fe3d8adc1c8ce055488f8a713e345
Marwan <marwanmo475@gmail.com>
2025-02-05 14:43:43 -0600

    added comments for set_registers

--- boot.c
+++ boot.c
@@ -11,6 +11,17 @@ int main(void) {
     while(1);
 }
 
+/* 
+ * argument 1 is in r0 takes function
+ * argument 2 is in r1 takes address for stack
+ *
+ * r13 is register for sp
+ *
+ * moves argument for r1 into sp register (r13)
+ *
+ * branches to function in r0
+ *
+ * */
 __attribute__((naked)) static void set_registers(uint32_t pc, uint32_t sp) {
     __asm("            \n\
             mov r13, r1\n\
