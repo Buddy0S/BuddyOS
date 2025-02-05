ed0624f23d18b4c79431eb32c1d66310ab0a11d2
Alexei Doell <alexeidoell@gmail.com>
2025-02-05 12:48:56 -0600

    fixed addresses

--- boot.c
+++ boot.c
@@ -1,10 +1,10 @@
 #include "boot.h"
 #include "memory_map.h"
-
+#include <stdint.h>
 
 #define __GPIO1_start__ 0x4804C000
-#define RESET_USERLED1 __GPIO1_start__ + 0x190
-#define SET_USERLED1 __GPIO1_start__ + 0x194
+#define RESET_USERLED1 0x4804C190
+#define SET_USERLED1 0x4804C194
 
 int main(void) {
     set_registers((uint32_t)buddy, __stack_start__);
@@ -18,9 +18,12 @@ __attribute__((naked)) static void set_registers(uint32_t pc, uint32_t sp) {
     ");
 }
 
+static inline void set_bit_sram(uint32_t addr, unsigned int bit) {
+        *(uint32_t*)((volatile char*)addr) |= 1 << bit;
+}
+
 void buddy(void) {
     while(1){
-        *((volatile char*)SET_USERLED1) = 0xFFFFFFFF;
-        *((volatile char*)RESET_USERLED1) = 0xFFFFFFFF;
+        *(volatile uint32_t*)((volatile char*)RESET_USERLED1) = 0xFFFFFFFF;
     }
 }
