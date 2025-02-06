7bc36dc95c9a397c242537ad3032f105ea38cf85
Marwan <marwanmo475@gmail.com>
2025-02-06 15:34:16 -0600

    set stack pointer for IRQ and FRQ

--- memory_map.h
+++ memory_map.h
@@ -4,5 +4,9 @@
 #include <stdint.h>
 extern uint32_t __sram_start__;
 extern uint32_t __sram_size__;
-extern uint32_t __stack_start__;
-extern uint32_t __stack_size__;
+extern uint32_t __stack_SVC_start__;
+extern uint32_t __stack_SVC_size__;
+extern uint32_t __stack_IRQ_start__;
+extern uint32_t __stack_IRQ_size__;
+extern uint32_t __stack_FRQ_start__;
+extern uint32_t __stack_FRQ_size__;
