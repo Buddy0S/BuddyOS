79ad30df7b9d3236eb33ac48932324733be8555d
Alexei Doell <alexeidoell@gmail.com>
2025-02-06 10:27:47 -0600

    dont even need a main function

--- boot.c
+++ boot.c
@@ -8,27 +8,6 @@
 #define LED2 23
 #define LED3 24
 
-int main(void) {
-    /* literally no point in doing this rn but it works */
-    set_registers((uint32_t)buddy, (uint32_t)&__stack_start__);
-}
-/* 
- * argument 1 is in r0 takes function
- * argument 2 is in r1 takes address for stack
- *
- * r13 is register for sp
- *
- * moves argument for r1 into sp register (r13)
- *
- * branches to function in r0
- *
- * */
-__attribute__((naked, noreturn)) static void set_registers(uint32_t pc, uint32_t sp) {
-    __asm("            \n\
-            mov sp, r1 \n\
-            bx r0     \n\
-    ");
-}
 void buddy(void) {
 
     volatile int i;
