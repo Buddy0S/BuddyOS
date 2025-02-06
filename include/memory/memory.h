cc5f6b8b01c8b023b6b4ef9f8e7dec233dc6f3b4
Diego Cabrera Pell <efm473@gemail.com>
2025-02-06 17:24:08 -0600

    Added UART and PRM addresses

---
+++ include/memory/memory.h
@@ -0,0 +1,19 @@
+/* Special memory addresses */
+
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#define PRM_DEV 0x44E00f00
+#define PRM_RTC 0x44E01000
+#define PRM_GFX 0x44E01100
+#define PRM_CEFUSE 0x44E01200
+
+#define UART0 0x44E09000
+#define UART1 0x48022000
+#define UART2 0x48024000
+#define UART3 0x481A6000
+#define UART4 0x481A8000
+#define UART5 0x481AA000
+
+
+#endif
