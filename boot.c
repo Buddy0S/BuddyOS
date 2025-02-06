0333e50e23e8d095d917912c960ed2481ac0b9e8
Marwan <marwanmo475@gmail.com>
2025-02-06 01:38:58 -0600

    can now turn on a chosen led

--- boot.c
+++ boot.c
@@ -6,6 +6,11 @@
 #define RESET_USERLED1 0x4804C190
 #define SET_USERLED1 0x4804C194
 
+#define LED0 21
+#define LED1 22
+#define LED2 23
+#define LED3 24
+
 int main(void) {
 
     buddy();
@@ -20,21 +25,25 @@ void initLED(void){
 
 }
 
-void LEDon(void){
+void LEDon(int led){
 
-    *(volatile uint32_t*)((volatile char*)SET_USERLED1) = (uint32_t)(0xF << 21);
+    uint32_t LEDstatus = *(volatile uint32_t*)((volatile char*)SET_USERLED1);
+
+    *(volatile uint32_t*)((volatile char*)SET_USERLED1) = LEDstatus | (uint32_t)(0x1 << led);
 }
 
-void LEDoff(void){
+void LEDoff(int led){
 
-    *(volatile uint32_t*)((volatile char*)RESET_USERLED1) = (uint32_t)(0xF << 21);
+    *(volatile uint32_t*)((volatile char*)RESET_USERLED1) = (uint32_t)(0x1 << led);
 }
 
 void buddy(void) {
 
     initLED();
     
-    LEDon();
+    LEDon(LED0);
+
+    LEDon(LED2);
 
     while(1);
 }
