18abed73859d1f1375cdc2ac8cb0e8f6951958e2
Alexei Doell <alexeidoell@gmail.com>
2025-02-06 14:13:15 -0600

    reverted led.c

--- led.c
+++ led.c
@@ -7,21 +7,14 @@ inline void initLED(void){
     *(volatile uint32_t*)((volatile char*)GLBL_GPIO_CLOCK + 0xAC) = 0x2;
     /* turn all gpio pins to output mode */
     *(volatile uint32_t*)((volatile char*)__GPIO1_start__ + IO_MODE_OFFSET) = 0x0;
-
 }
 
 inline void LEDon(int led){
-
-    volatile uint32_t LEDstatus = *(volatile uint32_t*)((volatile char*)SET_USERLED1);
-
-    *(volatile uint32_t*)((volatile char*)SET_USERLED1) = LEDstatus | (uint32_t)(0x1 << led);
+    *(volatile uint32_t*)((volatile char*)SET_USERLED1) = (uint32_t)(0x1 << led);
 }
 
 inline void LEDoff(int led){
-
-    volatile uint32_t LEDstatus = *(volatile uint32_t*)((volatile char*)RESET_USERLED1);
-
-    *(volatile uint32_t*)((volatile char*)RESET_USERLED1) = LEDstatus | (uint32_t)(0x1 << led);
+    *(volatile uint32_t*)((volatile char*)RESET_USERLED1) = (uint32_t)(0x1 << led);
 }
 
 
