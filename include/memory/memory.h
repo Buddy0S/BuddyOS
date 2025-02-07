40abb35957695c7594d14dd91efa448b9dff705a
Diego Cabrera Pell <efm473@gemail.com>
2025-02-06 18:50:58 -0600

    added Clock module and more PRM bases

--- include/memory/memory.h
+++ include/memory/memory.h
@@ -3,8 +3,22 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+/* Clock Module bases */
+#define CM_PER 0x44E00000
+#define CM_WKUP 0x44E00400
+#define CM_DPLL 0x44E00500
+#define CM_MPU 0x44E00600
+#define CM_DEVICE 0x44E00700
+#define CM_RTC 0x44E00800
+#define CM_GFX 0x44E00900
+#define CM_CEFUSE 0x44E00A00
 
-#define PRM_DEV 0x44E00f00
+/* Power Reset Module bases */
+#define PRM_IRQ 0x44E00B00
+#define PRM_PER 0x44E00C00
+#define PRM_WKUP 0x44E00D00
+#define PRM_MPU 0x44E00E00
+#define PRM_DEV 0x44E00F00
 #define PRM_RTC 0x44E01000
 #define PRM_GFX 0x44E01100
 #define PRM_CEFUSE 0x44E01200
