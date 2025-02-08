94faddb58cc444f0d15ca990a07e0806e08ce6fd
kan728 <kan728@usask.ca>
2025-02-07 20:11:45 -0600

    Source file that has the main code for the kernel

---
+++ kernel.c
@@ -0,0 +1,29 @@
+#include "memory.h"
+
+/* Define UART0 transmit register address (UART0 + offset) */
+#define UART0_THR (UART0 + UART_THR)
+
+/* Function to send one character over UART0 */
+void uart_send(char c) {
+    volatile unsigned int *uart_thr = (unsigned int *)UART0_THR;
+    /* In a full implementation, we're gonna have to check the UART status register here. */
+    *uart_thr = c;
+}
+
+/* Send a string over UART0. */
+void uart_print(const char *str) {
+    while (*str) {
+        uart_send(*str++);
+    }
+}
+
+void kernel_main(void) {
+    uart_print("Welcome to My Kernel!\n");
+    /* Loop forever to keep the kernel running */
+    while (1) { }
+}
+
+/* _start is the entry point that our linker script gonna reference */
+void _start(void) {
+    kernel_main();
+}
