06f401191dbc5300664ba275dd2503e8a2c89c5e
kevinzhang3 <kzhang366@gmail.com>
2025-02-09 01:10:57 +0000

    qemu memory map (omap35x)

---
+++ include/memory/qemu-mem.h
@@ -0,0 +1,11 @@
+#ifndef QEMU_MEM_H
+#define QEMU_MEM_H
+
+#define UART1       0x4806A000
+
+#define UART_THR    0x0
+#define UART_RHR    0x0
+#define UART_IER    0x4
+#define UART_FCR    0x8
+
+#endif
