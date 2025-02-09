5f1b18e495e19474301616cb63d6676f819044d3
kevinzhang3 <kzhang366@gmail.com>
2025-02-09 05:14:36 +0000

    broken sp

--- include/memory/qemu-mem.h
+++ include/memory/qemu-mem.h
@@ -1,10 +1,12 @@
 #ifndef QEMU_MEM_H
 #define QEMU_MEM_H
 
-#define UART1       0x4806A000
+#define UART1       0x49020000
 
 #define UART_THR    0x000
 #define UART_RHR    0x000
+#define UART_DLL    0x000
+#define UART_DLH    0x400
 #define UART_IER    0x400
 #define UART_FCR    0x800
 #define UART_LCR    0x00C
