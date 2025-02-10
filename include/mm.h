af19fada702602385adcec335c990c4e3a30a762
kevinzhang3 <kzhang366@gmail.com>
2025-02-10 06:53:56 +0000

    cookin up the buddy allocator :fire:

--- include/mm.h
+++ include/mm.h
@@ -1,11 +1,29 @@
 #ifndef MM_H
 #define MM_H
 
-#define KERNEL_START  0x80000000
-#define KERNEL_SIZE   0x00400000  /* 4MB for the time being */
-#define KERNEL_DYNAMIC     0x00400000  /* 4MB of dynamic mem */
-#define ALLOC_START   (KERNEL_START + KERNEL_SIZE + KERNEL_DYNAMIC)
-#define ALLOC_END     0x8FFFFFFF
+#define KERNEL_START        0x80000000
+#define KERNEL_SIZE         0x00400000  /* 4MB */
+#define KERNEL_RESERVED     0x00400000  /* 4MB */
+#define KERNEL_DYNAMIC      0x00400000  /* 4MB */
+
+#define KERNEL_TOTAL        (KERNEL_START + KERNEL_SIZE + KERNEL_RESERVED + KERNEL_DYNAMIC)
+
+#define KERNEL_RESERVED_START (KERNEL_START + KERNEL_SIZE)
+#define KERNEL_DYNAMIC_START (KERNEL_RESERVED_START + KERNEL_RESERVED)
+
+#define ALLOC_START         (KERNEL_START + KERNEL_TOTAL)
+#define ALLOC_END           0x8FFFFFFF
+
+/*
+ * data struct to store memory blocks
+ */
+struct mem_block {
+    bool used;
+    uint32_t addr;
+    struct free_list *next;
+
+};
+
 
 
 #endif
--- kernel/mm.c
+++ kernel/mm.c
@@ -1,16 +1,53 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include "mm.h"
 
 #define MAX_ORDER 15
 #define MIN_BLOCK 128
 
+/* addresses that we will be placing our data at */
+const uint32_t order_arr_addr = KERNEL_RESERVED_START;
+const uint32_t blocks_struct_addr = order_arr_addr + (sizeof(void *) * MAX_ORDER);
+
+/* we gotta allocate memory for the array of lists via raw memory reference */ 
+volatile struct mem_block **order_arr = (volatile struct mem_block **)order_arr_addr;
+/* same here with the actual structs */
+volatile struct mem_block *block_structs = (volatile struct mem_block *)blocks_struct_addr;
+uint32_t block_struct_iterator = 0;
+
 /*
- * data struct to store memory blocks
+ * data struct to store memory blocks as a linked list
  */
-struct mem_list {
+struct mem_block {
+    bool used;
     uint32_t addr;
     struct free_list *next;
 };
+
+/*
+ * returns a mem_block node
+ */
+struct mem_block *create_mem_block(void) {
+    return &block_structs[block_struct_iterator++];
+}
+
+/*
+ * we will have an array where each element corresponds to an order, each order
+ * then has a linked list of blocks belonging to that order
+ */
+void init_alloc(void) {
+    for (int i = 0; i < MAX_ORDER; i++) {
+        order_arr[i] = create_mem_block();
+        
+        order_arr[i]->used = 0;
+        order_arr[i]->addr = 0;
+        order_arr[i]->next = NULL;
+    }
+
+    /* initialize the largest starting block */
+    order_arr[0]->used = true;
+    order_arr[0]->addr = KERNEL_START + KERNEL_SIZE; 
+}
 
 /*
  * helper function for buddy allocator:
@@ -29,15 +66,20 @@ uint8_t find_order(size_t n) {
     return (uint8_t)__builtin_ctz(n);    /* return the order */
 }
 
-void init_alloc(void) {
-    struct mem_list blocks[MAX_ORDER];
-
-}
 
 void *kmalloc(size_t size) {
     uint8_t order;
-
     order = find_order(size);
+    
+    /* create the blocks of the order we need */
+    while (!order_arr[order]->addr) {
+        
+    }
+
+    /* loop through the list searching for the block */
+    while (order_arr[order]->next) {
+
+    }
     
     return NULL
 }
