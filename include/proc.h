874eb451005e3301bc725f5cfc956b2a2d98b1c1
kan728 <kan728@usask.ca>
2025-02-10 00:19:43 -0600

    Added PCB definition

--- include/proc.h
+++ include/proc.h
@@ -10,6 +10,13 @@ typedef enum {
     /* We can add more states like BLOCKED, etc, later */
 } ProcessState;
 
+/* Process Control Block (PCB) definition */
+typedef struct PCB {
+    int pid;              /* Process ID */
+    ProcessState state;   /* Process state */
+    uint32_t *stack_ptr;  /* Pointer to the saved context (stack pointer) */
+    uint32_t *stack_base; /* Base address of the allocated stack */
+} PCB;
 
-#endif // PROC_H
+#endif
 
