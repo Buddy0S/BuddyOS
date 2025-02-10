02a995afab9fbcb4f59ab4d46506bd1b48dc02ba
Marwan <marwanmo475@gmail.com>
2025-02-09 21:42:36 -0600

    think i got all the code down for clock init gotta test it now

--- boardinit/clock.c
+++ boardinit/clock.c
@@ -40,11 +40,41 @@ void pll_lock(uint32_t clckmode, uint32_t idlest){
 
 }
 
+void set_clock_frequency(uint32_t clcksel, int multi, int div){
+
+    /* 24MHz * multi / div + 1 */
+
+    uint32_t freq = (multi << 8) | (div);	
+    WRITE32(clcksel,freq);
+}
+
+void set_divider(uint32_t divider, uint32_t set, uint32_t clear){
+
+    uint32_t reg;
+
+    reg = READ32(divider);
+
+    reg &= ~clear;
+    reg |= set;
+
+    WRITE32(divider,reg);
+}
+
 /* TI manual 8.1.6.7.1 */
 void CORE_PLL_INIT(){
 
     /* Switch to bypass mode*/
     pll_to_bypass(CM_CLKMODE_DPLL_CORE, CM_IDLEST_DPLL_CORE);
+
+    /* Configure Multiply and divide */
+    set_clock_frequency(CM_CLKSEL_DPLL_CORE, 500, 23);
+
+    /* Set dividers */
+    set_divider(CM_DIV_M4_DPLL_CORE,10,0x1F);
+
+    set_divider(CM_DIV_M5_DPLL_CORE,8,0x1F);
+
+    set_divider(CM_DIV_M6_DPLL_CORE,4,0x1F);
 
     /* Lock PLL*/
     pll_lock(CM_CLKMODE_DPLL_CORE, CM_IDLEST_DPLL_CORE);
@@ -57,6 +87,12 @@ void MPU_PLL_INIT(){
     /* Switch to bypass mode*/
     pll_to_bypass(CM_CLKMODE_DPLL_MPU, CM_IDLEST_DPLL_MPU);
 
+    /* Configure Multiply and divide */
+    set_clock_frequency(CM_CLKSEL_DPLL_MPU, 1000, 23);
+
+    /* Set dividers */
+    set_divider(CM_DIV_M2_DPLL_MPU,1,0x1F);
+
     /* Lock PLL*/
     pll_lock(CM_CLKMODE_DPLL_MPU, CM_IDLEST_DPLL_MPU);
 }
@@ -67,6 +103,12 @@ void PER_PLL_INIT(){
     /* Switch to bypass mode*/
     pll_to_bypass(CM_CLKMODE_DPLL_PER, CM_IDLEST_DPLL_PER);
 
+    /* Configure Multiply and divide */
+    set_clock_frequency(CM_CLKSEL_DPLL_PER, 960, 23);
+
+    /* Set dividers */
+    set_divider(CM_DIV_M2_DPLL_PER,5,0x7F);
+
     /* Lock PLL*/
     pll_lock(CM_CLKMODE_DPLL_PER, CM_IDLEST_DPLL_PER);
 }
@@ -76,6 +118,12 @@ void DDR_PLL_INIT(){
 
     /* Switch to bypass mode*/
     pll_to_bypass(CM_CLKMODE_DPLL_DDR, CM_IDLEST_DPLL_DDR);
+
+    /* Configure Multiply and divide */
+    set_clock_frequency(CM_CLKSEL_DPLL_DDR, 400, 23);
+
+    /* Set dividers */
+    set_divider(CM_DIV_M2_DPLL_DDR,1,0x1F);
 
     /* Lock PLL*/
     pll_lock(CM_CLKMODE_DPLL_DDR, CM_IDLEST_DPLL_DDR);
