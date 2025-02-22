4ef2dcccb88dc1629261f6e9d0f6092384ef397a
Marwan <marwanmo475@gmail.com>
2025-02-21 18:39:05 -0600

    dude bootloader is done, some small issues with our gpio (uart/leds)

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,28 @@
 #include <stdint.h>
 #include "uart.h"
-
+#include "led.h"
+#include "reg.h"
 
 int main(){
 
-    //uart0_printf("Hello from kernel\n");
+    WRITE8(UART0_BASE, (uint8_t)'H');
+    WRITE8(UART0_BASE, (uint8_t)'I');
+    WRITE8(UART0_BASE, (uint8_t)' ');
+    WRITE8(UART0_BASE, (uint8_t)'F');
+    WRITE8(UART0_BASE, (uint8_t)'R');
+    WRITE8(UART0_BASE, (uint8_t)'O');
+    WRITE8(UART0_BASE, (uint8_t)'M');
+    WRITE8(UART0_BASE, (uint8_t)' ');
+    WRITE8(UART0_BASE, (uint8_t)'K');
+    WRITE8(UART0_BASE, (uint8_t)'E');
+    WRITE8(UART0_BASE, (uint8_t)'R');
+    WRITE8(UART0_BASE, (uint8_t)'N');
+    WRITE8(UART0_BASE, (uint8_t)'E');
+    WRITE8(UART0_BASE, (uint8_t)'L');
+    WRITE8(UART0_BASE, (uint8_t)'\n');
+
+
+    while(1){}	
 
     return 0;
 }
