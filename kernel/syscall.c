a2df25baa7c5ba3c476077c874b60306a358d7b5
Diego Cabrera Pell <diegocabrerapell@gmail.com>
2025-03-12 13:05:34 -0600

    added function from which all syscalls will be called from (not compilable and not added to main makefile)

---
+++ kernel/syscall.c
@@ -0,0 +1,13 @@
+#include <syscalls.h>
+#include <stdint.h>
+
+/* xv6 style syscalls */
+//static uint32_t (*syscalls[])(void) = {
+/* example:
+ * [SYS_FORK]   sys_fork, */
+//}
+
+/* modifes the context of the calling processs, sets EAX to the return value */
+void syscall(void) {
+
+}
