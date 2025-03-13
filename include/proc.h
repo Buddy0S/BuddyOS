0453063a012d0766698d091964be224dee9abcca
EmilyH <emlhk080804@gmail.com>
2025-03-12 19:20:15 -0600

    Added more fields to the PCB struct (ppid, children, priority, exitcode)

--- include/proc.h
+++ include/proc.h
@@ -2,6 +2,7 @@
 #define PROC_H
 
 #include <stdint.h>
+#include <list.h>
 
 #define MAX_PROCS   3
 #define STACK_SIZE  1024
@@ -11,21 +12,38 @@
 typedef enum {
     READY,
     RUNNING,
-    /* Additional states (e.g., BLOCKED) can be added later */
+    BLOCKED,
+    DEAD
 } ProcessState;
+
+/* Process priorities */
+typedef enum {
+  LOW,
+  MEDIUM,
+  HIGH
+} ProcessPriority;
 
 /* Process Control Block (PCB) definition */
 typedef struct PCB {
-    int pid;              /* Process ID */
-    ProcessState state;   /* Process state */
-    uint32_t *stack_ptr;  /* Pointer to the saved context (stack pointer) */
-    uint32_t *stack_base; /* Base address of the allocated stack */
+    int pid;                  /* Process ID */
+    int ppid;                 /* Parent process ID */
+    ProcessState state;       /* Process state */
+    ProcessPriority prio;      /* Process priority */
+
+    KNode children;            /* A list of this proc's children */
+
+    uint32_t *stack_ptr;      /* Pointer to the saved context (stack pointer) */
+    uint32_t *stack_base;     /* Base address of the allocated stack */
+
+    int exitCode;             /* Code/signal from when a process is interrupted */
+
+
 } PCB;
 
 /* Global process table and stacks */
-extern PCB pcb[MAX_PROCS];
-extern uint32_t proc_stacks[MAX_PROCS][STACK_SIZE];
-extern int current_index;
+extern PCB PROC_TABLE[MAX_PROCS];
+extern uint32_t PROC_STACKS[MAX_PROCS][STACK_SIZE];
+extern int CURRENT_INDEX;
 
 /* Function declarations */
 void delay(void);
