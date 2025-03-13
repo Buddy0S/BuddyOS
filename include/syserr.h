abcbdcc6f7ee677700eb03ff7d90755edf41f264
Diego Cabrera Pell <diegocabrerapell@gmail.com>
2025-03-12 20:02:07 -0600

    defined some error values for syscalls

---
+++ include/syserr.h
@@ -0,0 +1,14 @@
+/*
+ * A file for defining error numbers for use in syscalls
+ */
+
+#ifndef SYSERR_H
+#define SYSERR_H
+
+#define ESRCH   1       //No such process
+#define ENOMEM  2       //Not enough memory
+#define EINVAL  3       //Invalid argument
+#define ENOSYS  4       //Syscall not implemented
+#define EFAULT  5       //Bad memory address
+
+#endif
