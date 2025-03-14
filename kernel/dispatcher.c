af9b49d87ff9192fbee1869460b6f5a489356d7c
kevinzhang3 <kzhang366@gmail.com>
2025-03-13 23:13:26 -0600

    initial commit

---
+++ kernel/dispatcher.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include "uart.h"
+#include "proc.h"
+
+void dispatcher(void) {
+    // run first process
+    while (1) {
+        // svc handler gives code
+
+        // call corresponding routine
+    }
+}
