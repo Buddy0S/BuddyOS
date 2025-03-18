0afd29709694147f47969fdcc79eef4556f27944
Alexei Doell <alexeidoell@gmail.com>
2025-03-18 16:07:31 -0600

    i fucking hate all of you and this stupid fucking architecture

--- kernel/dispatcher.c
+++ kernel/dispatcher.c
@@ -8,14 +8,22 @@ void dispatcher(void) {
     // run first process
     while (1) {
         uart0_printf("start of dispatcher\n");
-        uart0_printf("kernel sp %x\n", kernel_process.stack_ptr);
+        uart0_printf("last process sp %x\n", current_process->stack_ptr);
+        uart0_printf("last process cpsr %x\n", current_process->cpsr);
+
         schedule();
         uart0_printf("next process sp %x\n", current_process->stack_ptr);
+        uart0_printf("next process cpsr %x\n", current_process->cpsr);
         uart0_printf("jumping to %x\n", current_process->context.lr);
 
 
+        if (current_process->started) {
+            switch_to_svc(kernel_process, current_process);    
+        } else {
+            current_process->started = true;
+            switch_to_start(kernel_process, current_process);    
+        }
 
-        switch_context(&kernel_process, current_process);    
     }
 }
 
--- kernel/k_intr.c
+++ kernel/k_intr.c
@@ -36,12 +36,27 @@ void svc_handler(uint32_t svc_num, uint32_t args[]) {
     uart0_printf("r3: %d\n", args[3]);
 
 
-    uart0_printf("kernel sp: %x\n", kernel_process.stack_ptr);
-    uart0_printf("kernel return addr: %x\n", kernel_process.context.lr);
-    uart0_printf("kernel r11: %x\n", kernel_process.context.r11);
-    switch_context(process, &kernel_process);
+    uart0_printf("kernel sp: %x\n", kernel_process->stack_ptr);
+    uart0_printf("kernel return addr: %x\n", kernel_process->context.lr);
 
+
+    uart0_printf("current sp: %x\n", sp);
+
+    switch_to_dispatch(current_process, kernel_process);
     uart0_printf("returned monkey\n");
+    /*
+    uart0_printf("current sp: %x\n", sp);
+        asm volatile("  \n\t    \
+                mov r11, lr     \n\t    \
+       " : : : "r11");
+
+    uart0_printf("current lr: %x\n", r11);
+
+        asm volatile("  \n\t    \
+                mrs r11, spsr     \n\t    \
+       " : : : "r11");
+    uart0_printf("current spsr: %x\n", r11);
+    */
 }
 
 
