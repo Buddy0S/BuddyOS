d236998149145eb8b531af2ed214a452c2922d9a
Aaron DSouza <aad921@usask.ca>
2025-03-18 23:08:57 -0600

    function prototypes for string functions

---
+++ include/string.h
@@ -0,0 +1,10 @@
+#ifndef STRING_H
+#define STRING_H
+
+size_t strlen(const char *str);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
+int strcmp(const char *str1, const char *str2);
+int strncmp(const char *str1, const char *str2, size_t n);
+
+#endif
