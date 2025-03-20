cdbe1235fe98aba2e98b6ba0edd17d131f7c6431
Alexei Doell <alexeidoell@gmail.com>
2025-03-19 18:53:21 -0600

    i think syscalls just work completely now

--- kernel/dispatcher.c
+++ kernel/dispatcher.c
@@ -8,12 +8,18 @@ void dispatcher(void) {
     // run first process
     while (1) {
         uart0_printf("start of dispatcher\n");
+
+        register uint32_t r0 asm("r0");
+        asm volatile("  \n\t    \
+                mov r0, pc     \n\t    \
+       " : : : "r0");
+
+        uart0_printf("current pc ish: %x\n", r0);
+
         uart0_printf("last process sp %x\n", current_process->stack_ptr);
-        uart0_printf("last process cpsr %x\n", current_process->cpsr);
 
         schedule();
         uart0_printf("next process sp %x\n", current_process->stack_ptr);
-        uart0_printf("next process cpsr %x\n", current_process->cpsr);
         uart0_printf("jumping to %x\n", current_process->context.lr);
 
 
