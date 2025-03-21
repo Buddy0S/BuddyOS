00a61e39b4dc2a1178ecea7979c896defa26bcd8
Diego Cabrera Pell <diegocabrerapell@gmail.com>
2025-03-21 13:11:03 -0600

    fixed macro not reading defined numbers correctly

--- include/syscall.h
+++ include/syscall.h
@@ -1,10 +1,12 @@
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
+#define STR(x) #x
+
 #define SYSCALL(x)                          \
 ({                                          \
     register int return_val asm("r0");      \
-    asm volatile (" svc #" #x "  \n\t");    \
+    asm volatile (" svc #" STR(x) "  \n\t");    \
     return_val;                             \
 })                                          \
 
