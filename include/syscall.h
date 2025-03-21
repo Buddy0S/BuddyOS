d1e205aa32c71e9d6d8eb8c25cefa5138539cd5d
Diego Cabrera Pell <diegocabrerapell@gmail.com>
2025-03-20 20:49:26 -0600

    added numbers for IPC syscalls

--- include/syscall.h
+++ include/syscall.h
@@ -19,5 +19,9 @@
 #define SYSCALL_FORK_NR         3
 #define SYSCALL_KILL_NR         4
 #define SYSCALL_GETPID_NR       5
+#define SYSCALL_SEND_NR         6
+#define SYSCALL_RECEIVE_NR      7
+#define SYSCALL_REPLY_NR        8
+#define SYSCALL_MSG_WAITING_NR  9
 
 #endif
