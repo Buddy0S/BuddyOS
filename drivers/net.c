b9e91cf5157d36b32553546a5c39df5d6800cf88
Marwan <marwanmo475@gmail.com>
2025-03-26 03:59:36 -0600

    transmit queue idea working

--- drivers/net.c
+++ drivers/net.c
@@ -462,6 +462,10 @@ extern void buddy();
 #define MAX_SOCKETS 8
 #define MAX_PENDING_PACKETS 10
 
+#define PING_REQ 1 
+#define SOCKET_REQ 2
+#define MAX_TRANSMIT_REQUEST 10
+
 /* ----------------------------STRUCTS----------------------------- */
 
 /* CPDMA header discriptors */
@@ -578,6 +582,27 @@ struct socket {
 };
 
 struct socket socket_table[MAX_SOCKETS];
+
+struct transmit_req {
+
+  int req_type;
+  int socket_num;
+  uint8_t* frame;
+  int size;
+  // for icmp
+  uint32_t ip;
+  uint8_t* mac;
+
+};
+
+struct transmit_queue {
+
+  struct transmit_req req_queue[MAX_TRANSMIT_REQUEST];
+  int req_pending;
+
+};
+
+struct transmit_queue transmit_que;
 
 /* --------------------------CPSW CODE----------------------------- */
 
@@ -2419,7 +2444,7 @@ void socket_unbind(int socket_num){
  * */
 struct payload socket_recv(int socket_num){
 
-  uint8_t data_index = socket_table[socket_num].packets_pending;
+  uint8_t data_index = socket_table[socket_num].packets_pending - 1;
 
   struct payload data = socket_table[socket_num].data[data_index];
 
@@ -2441,6 +2466,94 @@ void socket_send(int socket_num, uint8_t* frame, int size){
       break;
   }
 }
+/* --------------------------FUSION--------------------------- */
+
+/*
+ * creates and ands transmit request to queue
+ *
+ * */
+int transmit_request(uint8_t* frame, int size, int req_type, int socket_num, uint32_t ip, uint8_t* mac){
+
+  int index = transmit_que.req_pending;
+
+  if (index >= MAX_TRANSMIT_REQUEST){
+    return -1;
+  }
+
+  transmit_que.req_queue[index].req_type = req_type;
+  transmit_que.req_queue[index].socket_num = socket_num;
+  transmit_que.req_queue[index].frame = frame;
+  transmit_que.req_queue[index].size = size;
+  transmit_que.req_queue[index].ip = ip;
+  transmit_que.req_queue[index].mac = mac;
+
+  transmit_que.req_pending += 1;
+
+  return 1;
+
+}
+
+int socket_transmit_request(int socket_num, uint8_t* frame, int size){
+
+  return transmit_request(frame, size, SOCKET_REQ, socket_num, 0, 0);
+
+}
+
+int ping_request(uint32_t ip, uint8_t* mac){
+
+  uint8_t* mac_copy = (uint8_t*) kmalloc(MAC_ADDR_LEN);
+
+  net_memcopy(mac_copy, mac, MAC_ADDR_LEN);
+
+  return transmit_request(0,0,PING_REQ,0,ip,mac_copy);
+
+}
+
+void handle_transmit_request(struct transmit_req* req){
+
+  int req_type = req->req_type;
+
+  switch (req_type) {
+
+    case PING_REQ:
+      {
+        icmp_echo_request(req->ip,req->mac);
+        kfree(req->mac);
+      }
+      break;
+
+    case SOCKET_REQ:
+      {
+        socket_send(req->socket_num,req->frame,req->size);
+        kfree(req->frame);
+      }
+      break;
+  }
+
+}
+
+int transmit(){
+
+  int index;
+  int req_pending = transmit_que.req_pending;
+  struct transmit_req* cur_req;
+
+  if (req_pending == 0){
+    return 0;
+  }
+
+  while ( req_pending > 0){
+    index = req_pending - 1;
+    cur_req = &(transmit_que.req_queue[index]);
+    handle_transmit_request(cur_req); 
+    req_pending -= 1;
+  }
+
+  transmit_que.req_pending = req_pending;
+
+  return 1;
+
+}
 
 /* --------------------------INIT----------------------------- */
 /*
@@ -2457,6 +2570,8 @@ void init_network_stack(){
 
     init_sockets();
 
+    transmit_que.req_pending = 0;
+
     eth_interface.ip_addr = STATIC_IP;
 
     buddy();
@@ -2467,7 +2582,7 @@ void init_network_stack(){
     arp_anounce();
     arp_garp();
 
-    icmp_echo_request(gateway_ip,gateway_mac);
+    ping_request(gateway_ip,gateway_mac);
 
     int soc = socket(0,80,80,gateway_mac,gateway_ip,UDP,0);
 
@@ -2477,9 +2592,8 @@ void init_network_stack(){
       cpsw_recv();
       buddy();
       uint8_t* frame = (uint8_t*) kmalloc(128);
-      socket_send(soc,frame,128);
-      kfree(frame);
-
+      socket_transmit_request(soc,frame,128);
+      transmit();
     }
 
 }
