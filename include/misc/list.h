5fabc7fdc4cba0c8914573de48b4d34e54830371
Diego Cabrera Pell <diegocabrerapell@gmail.com>
2025-03-31 00:37:17 -0600

    added function docs to list functions

--- include/misc/list.h
+++ include/misc/list.h
@@ -21,35 +21,46 @@ struct KListIter {
 
 
 /* 
- * Does pointer arithmetic to get the pointer of the struct containing
- * the attribute pointer to by ptr
+ * Does pointer arithmetic to get the pointer of to the struct that contains
+ * the attribute.
  *
- * example:
- *  struct tclass {
- *      int attr1;
- *      int attr2;
- *  }
- *
- *  struct tclass t;
- *
- *  x = &t.attr2;
- *
- *  Then get_container(x, struct tclass, attr2) will equal &t
+ * Arguments:
+ *      (KNode*) entry_ptr: A pointer to a KNode attribute in a struct.
+ *      struct: The name of the struct that holds the attribute 
+ *          which entry_pointer points to.
+ *      attr: The name of the attribute that entry_ptr points to.
  *
  *  THIS ASSUMES YOU ARE RIGHT ABOUT THE ATTRIBUTE AND CLASS BE CAREFUL!!!!
  */
 #define knode_data(entry_ptr, struc, attr) \
     (struc*)((char*) entry_ptr - offsetof(struc, attr));
 
+
+/*
+ * Initializes a list as empty
+ */
 static inline void list_init(struct KList *list) {
     list->next = list;
     list->prev = list;
 }
 
+
+/*
+ * Returns 1 if list is empty else 0
+ */
 static inline int list_empty(struct KList *list) {
     return list->next == list;
 }
 
+
+/*
+ * Adds a node to list at the head of the list.
+ *
+ * Args:
+ *      (struct KList*) list: the list that is being added to.
+ *      (KNode) node: The node to be added (the node needs to be
+ *          an attribute of a class)
+ */
 static inline void list_add(struct KList *list, KNode *node) {
     node->next = list->next;
     node->prev = list;
@@ -57,6 +68,15 @@ static inline void list_add(struct KList *list, KNode *node) {
     list->next = node;
 }
 
+
+/*
+ * Adds a node to list at the tail of the list.
+ *
+ * Args:
+ *      (struct KList*) list: the list that is being added to.
+ *      (KNode) node: The node to be added (the node needs to be
+ *          an attribute of a class)
+ */
 static inline void list_add_tail(struct KList *list, KNode *node) {
     node->prev = list->prev;
     node->next = list;
@@ -64,27 +84,67 @@ static inline void list_add_tail(struct KList *list, KNode *node) {
     list->prev = node;
 }
 
+
+/*
+ * Pops a node out of the list it is in.
+ *
+ * ASSUMPTION: assumnes the know is actually in a list.
+ *
+ * Args:
+ *      (KNode*) node: A pointer to the node being popped.
+ *
+ * Returns a pointer to the node that was popped (same as the inputted node).
+ */
 static inline KNode* knode_pop(KNode* node) {
     node->prev->next = node->next;
     node->next->prev = node->prev;
     return node;
 }
 
+
+/*
+ * Pops a node out of a list from the head.
+ *
+ * Args:
+ *      (struct KList*) list: the list to pop a node out of.
+ *
+ * Returns a pointer to the popped knode.
+ */
 static inline KNode* list_pop(struct KList *list) {
     return knode_pop(list->next);
 }
 
+
+/*
+ * Pops a node out of a list from the tail.
+ *
+ * Args:
+ *      (struct KList*) list: the list to pop a node out of.
+ *
+ * Returns a pointer to the popped knode.
+ */
 static inline KNode* list_pop_tail(struct KList *list) {
     return knode_pop(list->prev);
 }
 
+
+/*
+ * Returns a pointer to the first knode in the list.
+ */
 static inline KNode* list_first(struct KList *list) {
     return list->next;
 }
 
+
+/*
+ * Returns a pointer to the last knode in the list.
+ */
 static inline KNode* list_last(struct KList *list) {
     return list->prev;
 }
+
+
+
 
 static inline void li_go_first(struct KListIter *iter) {
     iter->cursor = iter->list->next;
